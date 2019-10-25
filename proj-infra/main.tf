14ad6e275f465afeb7f1c0d99af0baf51769b246
Martin Pankraz <mapankra@microsoft.com>
2019-10-25 11:28:02 +0100

    initial tf commit

---
+++ proj-infra/main.tf
@@ -0,0 +1,12 @@
+  
+resource "azurerm_resource_group" "example" {
+  name     = "${var.prefix}-resources"
+  location = "${var.location}"
+}
+
+resource "azurerm_container_registry" "example" {
+  name                = "${var.prefix}registry"
+  resource_group_name = "${azurerm_resource_group.example.name}"
+  location            = "${azurerm_resource_group.example.location}"
+  sku                 = "Standard"
+}
